1ba15bd747059936c823dd89e8e40dddd5dc2b30
zion <lce322@naver.com>
2021-10-29 02:13:20 +0900

    width size change

--- savper/client/src/components/DocList.js
+++ savper/client/src/components/DocList.js
@@ -9,7 +9,9 @@ const DocList = () => {
       .catch((err) => console.error(err));
   },[]);
   const docList = docs.map((doc) =>
-  <img style ={{width:"100%"}} src={`http://localhost:5000/uploads/${doc.key}`}/>)
+  <img style ={{width:"100%"}} 
+  key = {doc.key}
+  src={`http://localhost:5000/uploads/${doc.key}`}/>)
   return (
     <div>
       <h3>DocList</h3>
