258951d9fe167755091124e1f1adde534e4d59d3
zion <lce322@naver.com>
2021-11-03 00:02:20 +0900

    userRegister

--- savper/server/models/User.js
+++ savper/server/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true}
+    hashedPassword: { type: String, required: true}
   },
   { timestamps: true } //생성, 업데이트 날짜 자동 저장
 );
