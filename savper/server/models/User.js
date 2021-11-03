b84cd1e2bb3962e208429a934ddd9774d2652703
zion <lce322@naver.com>
2021-11-04 02:09:10 +0900

    login

--- savper/server/models/User.js
+++ savper/server/models/User.js
@@ -4,7 +4,10 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    hashedPassword: { type: String, required: true}
+    hashedPassword: { type: String, required: true},
+    sessions:[{
+        createdAt:{type:Date, required:true},
+    },],
   },
   { timestamps: true } //생성, 업데이트 날짜 자동 저장
 );
